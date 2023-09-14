944c7d1e407c065a30307149518fa53ce437a247
Junichi Kato <j5ik2o@gmail.com>
2023-09-14 14:14:04 +0900

    refactor: EventStoreAsync

--- src/test/scala/com/github/j5ik2o/event/store/adapter/scala/internal/DynamoDBUtils.scala
+++ src/test/scala/com/github/j5ik2o/event/store/adapter/scala/internal/DynamoDBUtils.scala
@@ -2,10 +2,12 @@ package com.github.j5ik2o.event.store.adapter.scala.internal
 
 import software.amazon.awssdk.auth.credentials.{ AwsBasicCredentials, StaticCredentialsProvider }
 import software.amazon.awssdk.regions.Region
-import software.amazon.awssdk.services.dynamodb.DynamoDbClient
 import software.amazon.awssdk.services.dynamodb.model._
+import software.amazon.awssdk.services.dynamodb.{ DynamoDbAsyncClient, DynamoDbClient }
 
 import java.net.URI
+import scala.concurrent.{ ExecutionContext, Future }
+import scala.jdk.FutureConverters.CompletionStageOps
 
 object DynamoDBUtils {
 
@@ -16,6 +18,20 @@ object DynamoDBUtils {
       region: Region
   ): DynamoDbClient = {
     DynamoDbClient
+      .builder()
+      .endpointOverride(URI.create(endpointForDynamoDB))
+      .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKeyId, secretAccessKey)))
+      .region(region)
+      .build();
+  }
+
+  def dynamodbAsyncClient(
+      endpointForDynamoDB: String,
+      accessKeyId: String,
+      secretAccessKey: String,
+      region: Region
+  ): DynamoDbAsyncClient = {
+    DynamoDbAsyncClient
       .builder()
       .endpointOverride(URI.create(endpointForDynamoDB))
       .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKeyId, secretAccessKey)))
@@ -45,6 +61,33 @@ object DynamoDBUtils {
     )
   }
 
+  def createJournalTableAsync(
+      client: DynamoDbAsyncClient,
+      tableName: String,
+      indexName: String
+  )(implicit ec: ExecutionContext): Future[Unit] = {
+    val pt = ProvisionedThroughput.builder.readCapacityUnits(10L).writeCapacityUnits(5L).build
+    client
+      .createTable(
+        CreateTableRequest.builder
+          .tableName(tableName).attributeDefinitions(
+            AttributeDefinition.builder.attributeName("pkey").attributeType(ScalarAttributeType.S).build,
+            AttributeDefinition.builder.attributeName("skey").attributeType(ScalarAttributeType.S).build,
+            AttributeDefinition.builder.attributeName("aid").attributeType(ScalarAttributeType.S).build,
+            AttributeDefinition.builder.attributeName("seq_nr").attributeType(ScalarAttributeType.N).build
+          ).keySchema(
+            KeySchemaElement.builder.attributeName("pkey").keyType(KeyType.HASH).build,
+            KeySchemaElement.builder.attributeName("skey").keyType(KeyType.RANGE).build
+          ).globalSecondaryIndexes(
+            GlobalSecondaryIndex.builder
+              .indexName(indexName).keySchema(
+                KeySchemaElement.builder.attributeName("aid").keyType(KeyType.HASH).build,
+                KeySchemaElement.builder.attributeName("seq_nr").keyType(KeyType.RANGE).build
+              ).projection(Projection.builder.projectionType(ProjectionType.ALL).build).provisionedThroughput(pt).build
+          ).provisionedThroughput(pt).build
+      ).asScala.map(_ => ())
+  }
+
   def createSnapshotTable(client: DynamoDbClient, tableName: String, indexName: String): Unit = {
     val pt = ProvisionedThroughput.builder.readCapacityUnits(10L).writeCapacityUnits(5L).build
     client.createTable(
@@ -71,5 +114,38 @@ object DynamoDBUtils {
           TimeToLiveSpecification.builder.enabled(true).attributeName("ttl").build
         ).build
     )
+  }
+
+  def createSnapshotTableAsync(client: DynamoDbAsyncClient, tableName: String, indexName: String)(implicit
+      ec: ExecutionContext
+  ): Future[Unit] = {
+    val pt = ProvisionedThroughput.builder.readCapacityUnits(10L).writeCapacityUnits(5L).build
+    client
+      .createTable(
+        CreateTableRequest.builder
+          .tableName(tableName).attributeDefinitions(
+            AttributeDefinition.builder.attributeName("pkey").attributeType(ScalarAttributeType.S).build,
+            AttributeDefinition.builder.attributeName("skey").attributeType(ScalarAttributeType.S).build,
+            AttributeDefinition.builder.attributeName("aid").attributeType(ScalarAttributeType.S).build,
+            AttributeDefinition.builder.attributeName("seq_nr").attributeType(ScalarAttributeType.N).build
+          ).keySchema(
+            KeySchemaElement.builder.attributeName("pkey").keyType(KeyType.HASH).build,
+            KeySchemaElement.builder.attributeName("skey").keyType(KeyType.RANGE).build
+          ).globalSecondaryIndexes(
+            GlobalSecondaryIndex.builder
+              .indexName(indexName).keySchema(
+                KeySchemaElement.builder.attributeName("aid").keyType(KeyType.HASH).build,
+                KeySchemaElement.builder.attributeName("seq_nr").keyType(KeyType.RANGE).build
+              ).projection(Projection.builder.projectionType(ProjectionType.ALL).build).provisionedThroughput(pt).build
+          ).provisionedThroughput(pt).build
+      ).asScala.flatMap { _ =>
+        client
+          .updateTimeToLive(
+            UpdateTimeToLiveRequest.builder
+              .tableName(tableName).timeToLiveSpecification(
+                TimeToLiveSpecification.builder.enabled(true).attributeName("ttl").build
+              ).build
+          ).asScala
+      }.map(_ => ())
   }
 }
