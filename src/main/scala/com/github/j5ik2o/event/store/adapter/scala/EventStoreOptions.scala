00e3806125d075feb4c289ad5fa149bdd9233930
Junichi Kato <j5ik2o@gmail.com>
2023-09-22 16:21:19 +0900

    fix: add DeserializationException support

--- src/main/scala/com/github/j5ik2o/event/store/adapter/scala/EventStoreOptions.scala
+++ src/main/scala/com/github/j5ik2o/event/store/adapter/scala/EventStoreOptions.scala
@@ -14,8 +14,21 @@ import scala.concurrent.duration.FiniteDuration
 trait EventStoreOptions[AID <: AggregateId, A <: Aggregate[A, AID], E <: Event[AID]] {
   type This <: EventStoreOptions[AID, A, E]
 
+  /** Specifies the number of snapshots to keep. / スナップショットを保持する数を指定します。
+    *
+    * @param keepSnapshotCount
+    *   the number of snapshots kept / スナップショットを保持する数
+    * @return
+    *   [[This]]
+    */
   def withKeepSnapshotCount(keepSnapshotCount: Int): This
 
+  /** Specifies the time until it is deleted by TTL. / TTLによって削除されるまでの時間を指定します。
+    *
+    * @param deleteTtl
+    *   Time until it is deleted by TTL / TTLによって削除されるまでの時間
+    * @return
+    */
   def withDeleteTtl(deleteTtl: FiniteDuration): This
 
   def withKeyResolver(keyResolver: KeyResolver[AID]): This
